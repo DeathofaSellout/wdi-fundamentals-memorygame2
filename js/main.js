9d030406c5569133f4ce792071f69632e1b79ee3
Erick Recher <javascriptforlife@gmail.com>
2020-11-08 21:19:58 -0800

    more game logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2){
-  if(cardOne === cardTwo){
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
-  }
+function checkForMatch(){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+ 	console.log("You found a match!");
+ } else {
+ 	console.log("Sorry, try again.");
+ }
 }
+function flipCard(cardId){
+  checkForMatch();
+  if (cardsInPlay.length === 2){
+    if(cardOne === cardTwo){
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
+  }
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
+// if (cardsInPlay.length === 2){
+//   if(cardOne === cardTwo){
+//     alert("You found a match!");
+//   } else {
+//     alert("Sorry, try again.");
+//   }
+// }
