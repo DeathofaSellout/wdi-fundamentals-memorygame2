4b0940c74cd21c63ba3edbc6b416e2b722197d8e
Erick Recher <javascriptforlife@gmail.com>
2020-11-08 20:34:18 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2){
+  if(cardOne === cardTwo){
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
